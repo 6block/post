a9516420099903f33315f99f19aefa12bd768100
scuwan <215587407@qq.com>
2023-12-05 18:56:37 +0800

    export some postrs api

---
+++ expostrs/api.go
@@ -0,0 +1,46 @@
+package expostrs
+
+import (
+	"github.com/spacemeshos/post/internal/postrs"
+	"go.uber.org/zap"
+)
+
+type VerifyPosOptionsFunc = postrs.VerifyPosOptionsFunc
+
+type ScryptParams = postrs.ScryptParams
+
+type Provider = postrs.Provider
+
+const ClassGPU = postrs.ClassGPU
+
+const ClassCPU = postrs.ClassCPU
+
+var ErrInvalidPos = postrs.ErrInvalidPos
+
+func OpenCLProviders() ([]Provider, error) {
+	return postrs.OpenCLProviders()
+}
+
+func NewScryptParams(n, r, p uint) ScryptParams {
+	return postrs.TranslateScryptParams(n, r, p)
+}
+
+func ToFile(toFile uint32) VerifyPosOptionsFunc {
+	return postrs.ToFile(toFile)
+}
+
+func VerifyPos(dataDir string, scryptParams ScryptParams, o ...VerifyPosOptionsFunc) error {
+	return postrs.VerifyPos(dataDir, scryptParams, o...)
+}
+
+func WithFraction(fraction float64) VerifyPosOptionsFunc {
+	return postrs.WithFraction(fraction)
+}
+
+func FromFile(fromFile uint32) VerifyPosOptionsFunc {
+	return postrs.FromFile(fromFile)
+}
+
+func VerifyPosWithLogger(logger *zap.Logger) VerifyPosOptionsFunc {
+	return postrs.VerifyPosWithLogger(logger)
+}
